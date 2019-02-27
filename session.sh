cac5b92577285a494c9a9fbb9b8dad427b3dd9db
Chris Elford <chris.n.elford@gmail.com>
2019-02-27 19:52:16 +0000

    First Script

---
+++ session.sh
@@ -0,0 +1,32 @@
+
+```python
+import urllib
+help(urllib)
+
+exit
+exit()
+y
+```
+
+
+```ipython
+!pip install PyGithub
+from github import Github
+help(Github)
+Github?
+
+Github()
+
+g = _
+
+g.get_repo('rust-lang/rust')
+
+_?
+_??
+
+repo = _23
+
+repo.has_issues()
+repo.has_issues
+```
+
